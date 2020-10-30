d9a28100fbc04f10f7cff37beddc59c2b1ee1f30
Leandroeinfo <leandroeinfo@gmail.com>
2020-10-30 15:12:04 -0300

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Kubctiel"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
